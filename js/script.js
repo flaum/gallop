b98ef47fbed76203f81c85c0cae2d7c49d44a924
flaum <skflaum@gmail.com>
2016-08-26 21:15:38 +0300

    Последняя версия 19:36 26.08

--- js/script.js
+++ js/script.js
@@ -1,39 +1,48 @@
-var links = $(document).find("a");
-var leftSidebar = $(".left-sidebar").children();
-var leftSidebarItem = leftSidebar.children();
-var userBlock = $(".left-sidebar__user-block");
-var loginBtns = $(".left-sidebar__enter-btns").children();
-var popupOuter = $(".popup-outer");
-var btnClass;
 $(document).ready(function() {
+	var links = $(document).find("a");
+	var leftSidebar = $(".left-sidebar").children();
+	var leftSidebarItem = leftSidebar.children();
+	var userBlock = $(".left-sidebar__user-block");
+	var loginBtns = $(".left-sidebar__enter-btns").children();
+	var popupOuter = $(".popup-outer");
 
 
 
-	// left sidebar popup mode function
+	// popup mode function
 	function popupMode(element) {
-		// btnClass = $(element).attr("class").slice(0, ($(element).attr("class").indexOf(" ") + 1));
-		btnClass = $(element).attr("class");
+		var btnClass = $(element).attr("class");
+
+		for(var i = 0; i < popupOuter.children().length; i++) {
+			if($(popupOuter.children()[i]).hasClass("popup--current") == true && ($(popupOuter.children()[i]).hasClass("popup--login") == false || $(popupOuter.children()[i]).hasClass("popup--registration") == false)) {
+				$(popupOuter.children()[i]).removeClass("popup--current").addClass("popup--exit");
+			}
+		};
 
 		if(leftSidebar.parent().hasClass("left-sidebar--popup") == false) {
 			leftSidebar.parent().addClass("left-sidebar--popup");
 		};
 
-		$(element).addClass(btnClass + "--active");
-
 		popupOuter.removeClass("popup-outer--exit").addClass("popup-outer--visible");
 
 		if(btnClass  == "left-sidebar__login-btn") {
+			$(element).toggleClass("left-sidebar__login-btn--active");
 			$(".left-sidebar__sign-btn").removeClass("left-sidebar__sign-btn--active")
-			$(".popup--registration").removeClass("popup--current").addClass("popup--exit");
+
+			if($(".popup--registration").hasClass("popup--current") == true) {
+				$(".popup--registration").removeClass("popup--current").addClass("popup--exit");
+			};
+
 			$(".popup--login").removeClass("popup--exit").addClass("popup--current");
 
-			console.log("allGood");
-		} else {
+		} else if (btnClass  == "left-sidebar__sign-btn") {
+			$(element).toggleClass("left-sidebar__sign-btn--active");
 			$(".left-sidebar__login-btn").removeClass("left-sidebar__login-btn--active");
-			$(".popup--login").removeClass("popup--current").addClass("popup--exit");
-			$(".popup--registration").removeClass("popup--exit").addClass("popup--current");
 
-			console.log("allGood");
+			if($(".popup--login").hasClass("popup--current") == true) {
+				$(".popup--login").removeClass("popup--current").addClass("popup--exit");
+			};
+
+			$(".popup--registration").removeClass("popup--exit").addClass("popup--current");
 		};
 
 		for(var i = 0; i < leftSidebarItem.length; i++) {
@@ -49,8 +58,38 @@ $(document).ready(function() {
 				$(leftSidebarItem[i]).addClass(className);
 			};
 		};
+
+		return false;
 	};
 
+	// popup exit function
+	function popupExit() {
+		leftSidebar.parent().removeClass("left-sidebar--popup");
+		popupOuter.removeClass("popup-outer--visible").addClass("popup-outer--exit");
+
+		for(var i = 0; i < popupOuter.children().length; i++) {
+			if($(popupOuter.children()[i]).hasClass("popup--current") == true) {
+				$(popupOuter.children()[i]).removeClass("popup--current").addClass("popup--exit");
+			}
+		};
+
+		for(var i = 0; i < loginBtns.length; i++) {
+			var btnClass;
+
+			if($(loginBtns[i]).attr("class").indexOf(" ") == -1) {
+				btnClass = $(loginBtns[i]).attr("class") + "--active";
+			} else {
+				btnClass = $(loginBtns[i]).attr("class").slice(0, $(loginBtns[i]).attr("class").indexOf(" ")) + "--active";
+			};
+			$(loginBtns[i]).removeClass(btnClass);
+		};
+
+		for(var i = 0; i < leftSidebarItem.length; i++) {
+			// var className = $(leftSidebarItem[i]).attr("class").slice(0, $(leftSidebarItem[i]).attr("class").indexOf(" ")) + "--popup";
+			var className = $(leftSidebarItem[i]).attr("class").slice(0, $(leftSidebarItem[i]).attr("class").indexOf(" ")) + "--popup";
+			$(leftSidebarItem[i]).removeClass(className);
+		};
+	}
 
 	// if link dont use prevent default event
 	for(var i = 0; i < links.length; i++) {
@@ -74,30 +113,39 @@ $(document).ready(function() {
 		});
 	};
 
-	$(".popup__close-btn").on("click", function(e) {
-		e == e || event;
+	// переход к форме восстановления пароля
+
+	$(".popup--login").find(".popup__form-link").on("click", function(e) {
+		e = e || event;
 		e.preventDefault;
 
-		leftSidebar.parent().removeClass("left-sidebar--popup");
-		popupOuter.removeClass("popup-outer--visible").addClass("popup-outer--exit");
-
-		for(var i = 0; i < loginBtns.length; i++) {
-			var btnClass;
-
-			if($(loginBtns[i]).attr("class").indexOf(" ") == -1) {
-				btnClass = $(loginBtns[i]).attr("class") + "--active";
-			} else {
-				btnClass = $(loginBtns[i]).attr("class").slice(0, $(loginBtns[i]).attr("class").indexOf(" ")) + "--active";
-			};
-			$(loginBtns[i]).removeClass(btnClass);
-		};
-
-		for(var i = 0; i < leftSidebarItem.length; i++) {
-			// var className = $(leftSidebarItem[i]).attr("class").slice(0, $(leftSidebarItem[i]).attr("class").indexOf(" ")) + "--popup";
-			var className = $(leftSidebarItem[i]).attr("class").slice(0, $(leftSidebarItem[i]).attr("class").indexOf(" ")) + "--popup";
-			console.log(className);
-			$(leftSidebarItem[i]).removeClass(className);
-		};
+		$(".popup--login").removeClass("popup--current").addClass("popup--exit");
+		$(".popup--reset-main").removeClass("popup--exit").addClass("popup--current");
 	});
 
+	// переход с формы регистрации на форму входа
+
+	$(".popup--registration").find(".popup__form-link").on("click", function(e) {
+		e = e || event;
+		e.preventDefault;
+
+		$(".popup--registration").removeClass("popup--current").addClass("popup--exit");
+		$(".popup--login").removeClass("popup--exit").addClass("popup--current");
+		$(".left-sidebar__login-btn").addClass("left-sidebar__login-btn--active");
+		$(".left-sidebar__sign-btn").removeClass("left-sidebar__sign-btn--active");
+	});
+
+	// закрытие попапов
+	$(".popup__close-btn").on("click", function(e) {
+		e = e || event;
+		e.preventDefault;
+		popupExit();
+	});
+
+	$(this).keydown(function (e) {
+		e = e || event;
+		if(e.which == 27) {
+			popupExit();
+		}
+	});
 });
